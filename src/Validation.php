fb4f69c879398fdb63b369e471f7c29125bf5592
Ano Van <anovanmaximuz@gmail.com>
2023-09-04 16:12:10 +0700

    update

--- src/Validation.php
+++ src/Validation.php
@@ -63,25 +63,9 @@ class Validation{
                 }
             }
 
-            throw new validationHeaderException($exceptions[0]);
+            return $exceptions;
         }else{
             return true;
         }
-    }
-}
-
-class validationHeaderException extends Exception {
-    
-    /**
-     * Construct the exception. Note: The message is NOT binary safe.
-     * @link https://php.net/manual/en/exception.construct.php
-     * @param string $message [optional] The Exception message to throw.
-     * @param Throwable|null $previous [optional] The previous throwable used for the exception chaining.
-     * @param int $code [optional] The Exception code.
-     */
-    public function __construct(string $message = '', int $code = 422) {
-        parent::__construct($message, $code);
-        $this->message = "$message";
-        $this->code = $code;
     }
 }
