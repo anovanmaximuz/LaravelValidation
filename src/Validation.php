edc04c037cb077f98858f6e03e581498c47b44f0
Ano Van <anovanmaximuz@gmail.com>
2023-09-06 15:17:27 +0700

    udpate

--- src/Validation.php
+++ src/Validation.php
@@ -5,13 +5,21 @@ use Exception;
 
 class Validation extends Exception{
 
-    public static function validateHeaders($headers, $headerRoles=[]) {
-
+    public static function validateHeaders($headers, $headerRoles=[], $raw=false) {
+        //raw
+        //name 
+        //   -required=false
+        //   -type=false
+        //   -length=false
         $errors = [];
         $row = 0;
         foreach ($headerRoles as $header => $role){
             if (strpos($role, "required") !== false && !isset($headers[$header])) {
-                $errors[$row][] = "Header ".$header." must be set";
+                if($raw){
+                    $errors[$header]['required'] = false;
+                }else{
+                    $errors[$row][] = "Header ".$header." must be set";
+                }
             }
             
             if(isset($headers[$header])){
@@ -22,7 +30,11 @@ class Validation extends Exception{
                         if(count($mins)>0 && count($mins)==2){
                             $minValue = $mins[1];
                             if(strlen($headers[$header])<$minValue){
-                                $errors[$row][] = "Minimum ".$minValue." characters allowed for header '".$header."' ";
+                                if($raw){
+                                    $errors[$header]['length'] = false;
+                                }else{
+                                    $errors[$row][] = "Minimum ".$minValue." characters allowed for header '".$header."' ";
+                                }
                             }
                         }else{
                             $errors[$row][] = "Wrong minimum length format for header '".$header."', eq: min:5 ";
@@ -34,7 +46,11 @@ class Validation extends Exception{
                         if(count($maxs)>0 && count($maxs)==2){
                             $maxValue = $maxs[1];
                             if(strlen($headers[$header])>$maxValue){
-                                $errors[$row][] = "Maximum ".$maxValue." characters allowed for header '".$header."' ";
+                                if($raw){
+                                    $errors[$header]['length'] = false;
+                                }else{
+                                    $errors[$row][] = "Maximum ".$maxValue." characters allowed for header '".$header."' ";
+                                }
                             }
                         }else{
                             $errors[$row][] = "Wrong maximum length format for header '".$header."', eq: max:5 ";
@@ -49,14 +65,18 @@ class Validation extends Exception{
 
 
         if(count($errors)>0){
-            $exceptions = [];
-            foreach($errors as $error){
-                foreach($error as $logs){
-                    $exceptions[] = $logs;
+            if($raw){
+
+            }else{
+                $exceptions = [];
+                foreach($errors as $error){
+                    foreach($error as $logs){
+                        $exceptions[] = $logs;
+                    }
                 }
             }
 
-            return ['code'=>203, 'messages'=>$exceptions];
+            return ['code'=>203, 'messages'=>($raw) ? "errors":$exceptions,'data'=>$errors];
         }else{
             return ['code'=>200];
         }
